faf71ca5b6818c84b214cee79e979f2564622cd9
Hadas <zhadas83@gmail.com>
2022-07-10 14:09:17 +0300

    add try except

--- backend_testing.py
+++ backend_testing.py
@@ -16,16 +16,16 @@ if res.ok:
 else:
     print("test failed")
 
-# PUT – will modify existing username (in the database).
-res = requests.put('http://127.0.0.1:5000/user/1', json={"user_name": "george"})
-if res.ok:
-    print(res.json())
-else:
-    print("test failed")
-
-# DELETE – will delete existing user (from database).
-res = requests.delete('http://127.0.0.1:5000/user/1')
-if res.ok:
-    print(res.json())
-else:
-    print("test failed")
+# # PUT – will modify existing username (in the database).
+# res = requests.put('http://127.0.0.1:5000/user/1', json={"user_name": "george"})
+# if res.ok:
+#     print(res.json())
+# else:
+#     print("test failed")
+#
+# # DELETE – will delete existing user (from database).
+# res = requests.delete('http://127.0.0.1:5000/user/1')
+# if res.ok:
+#     print(res.json())
+# else:
+#     print("test failed")
