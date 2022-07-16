a74080b405311b3ba2066b2887387860e5d156e3
Hadas <zhadas83@gmail.com>
2022-07-17 00:44:55 +0300

    add try except

--- clean_environment.py
+++ clean_environment.py
@@ -1,4 +1,9 @@
 import requests
-
-requests.get('http://127.0.0.1:5000/stop_server')
-requests.get('http://127.0.0.1:5001/stop_server')
+try:
+    requests.get('http://127.0.0.1:5000/stop_server')
+except:
+    print("servers are not responding")
+try:
+    requests.get('http://127.0.0.1:5001/stop_server')
+except:
+    print("servers are not responding")
